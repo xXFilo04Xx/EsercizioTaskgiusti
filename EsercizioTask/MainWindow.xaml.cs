7ef5f3d5f1a710f7511fc8fe3ce810fd4dcc9dc4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-07 08:42:45 +0200

    Add project files.

---
+++ EsercizioTask/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EsercizioTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bntEsegui_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            ProgressBar.Minimum = 0;
+            ProgressBar.Maximum = 100;
+            ProgressBar.Value = 0;
+            Task t1 = Task.Factory.StartNew(() => Multipli(a));
+        }
+
+        private void Multipli(int a)
+        {
+            int d = 200000000;
+            int m = 0;
+            for (int i = 1; i <= d; i++)
+            {
+                if ((i % a) == 0)
+                {
+                    m++;
+                }
+                if (i % 2000000 == 0)
+                {
+                    ProgressBar.Dispatcher.Invoke(() =>
+                    ProgressBar.Value++);
+                };
+            }
+
+            lblMultipli.Dispatcher.Invoke(() =>
+                lblMultipli.Content = m
+            );
+        }
+    }
+}
